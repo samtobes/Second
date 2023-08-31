931abc019a2249a88e9fad56450d9cebcdac44e0
samtobes <samtobes007@gmail.com>
2023-08-31 16:16:19 +0100

    added another feature

--- NEWS/settings.py
+++ NEWS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://samtobes-da96b17b95b6.herokuapp.com/"]
 
 
 # Application definition
